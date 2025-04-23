abd5ce8f1970e503807ec4f20fab93866c4edf9c
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-04-23 16:04:58 +0100

    Add revision topics based off ease and dropdown for new flashcards

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,5 @@
 from app import db
-from app.models import User
+from app.models import User, FlashCard
 import sqlalchemy as sa
 
 
@@ -19,3 +19,20 @@ def reset_and_get_topic_info(user):
 
 def get_user_by_username(name):
     return db.session.scalar(sa.select(User).where(User.username == name))
+
+
+def calculate_ease(card):
+    percent = (card.times_correct / card.times_seen) * 100
+    return max(1, int(percent))
+
+
+def grade_card(card, correct):
+    card.times_seen += 1
+    if correct:
+        card.times_correct += 1
+    else:
+        card.times_wrong += 1
+
+    card.ease = calculate_ease(card)
+    db.session.commit()
+    return card
